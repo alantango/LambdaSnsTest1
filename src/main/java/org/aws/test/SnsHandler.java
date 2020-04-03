074bf1cad67e8186ace8c4905b8b1a51ed30d2d5
Alan Tang <alantango@gmail.com>
2020-04-02 22:44:42 -0400

    init load

---
+++ src/main/java/org/aws/test/SnsHandler.java
@@ -0,0 +1,23 @@
+package org.aws.test;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SNSEvent;
+
+public class SnsHandler implements RequestHandler<SNSEvent, Object> {
+
+    @Override
+    public String handleRequest(SNSEvent request, Context context) {
+        String timeStamp = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss").format(Calendar.getInstance().getTime());
+        context.getLogger().log("Invocation started: " + timeStamp);
+        context.getLogger().log(request.getRecords().get(0).getSNS().getMessage());
+
+        timeStamp = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss").format(Calendar.getInstance().getTime());
+        context.getLogger().log("Invocation completed: " + timeStamp);
+        return null;
+    }
+
+}
